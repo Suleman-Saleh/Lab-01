c991854d6f5125de121ed728a424802467fe9148
Suleman-Saleh <msgul85@gmail.com>
2023-09-30 03:30:20 -0700

    LAb-04

---
+++ src/Week_04/Card_Test.java
@@ -0,0 +1,12 @@
+package Week_04;
+
+public class Card_Test {
+
+    public static void main(String[] args){
+        Deck deck = new Deck();
+        deck.shuffle();
+        deck.displayCards();
+
+    }
+
+}
