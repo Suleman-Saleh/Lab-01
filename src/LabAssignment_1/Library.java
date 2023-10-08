7e2d4d9003ddf0a49196929cce736c716c171946
Suleman-Saleh <msgul85@gmail.com>
2023-10-08 05:00:13 -0700

    Merge remote-tracking branch 'origin/master'

--- src/LabAssignment_1/Library.java
+++ src/LabAssignment_1/Library.java
@@ -1,5 +1,4 @@
 package LabAssignment_1;
-<<<<<<< HEAD
 
 public class Library {
     private LibraryItem[] libraryItems = new LibraryItem[100];
@@ -27,6 +26,7 @@ public class Library {
     }
 
     public void displayBorrowedItems() {
+        System.out.println("Borrowed Items:");
         for (int i = 0; i < itemCount; i++) {
             if (libraryItems[i].isBorrowed()) {
                 System.out.println(libraryItems[i]);
@@ -34,42 +34,3 @@ public class Library {
         }
     }
 }
-=======
-public class Library {
-    LibraryItem[] libraryItems = new LibraryItem[100];
-    private int itemcount = 0;
-
-
-    public void addItem(LibraryItem add1){
-        libraryItems[itemcount++] = add1;
-    }
-
-    public void borrowItem(LibraryItem item){
-
-        item.setIsborrowed(true);
-    }
-
-    public void returnItem(LibraryItem item){
-        item.setIsborrowed(false);
-    }
-
-    public void displayBorrowedItems(){
-        for(int i=0;i<itemcount;i++){
-            if(libraryItems[i].isBorrowed()){
-                System.out.println(libraryItems[i]);
-            }
-
-        }
-    }
-
-    public void displayAvailableItems(){
-        for(int i=0;i<itemcount;i++){
-            if(!libraryItems[i].isBorrowed()){
-                System.out.println(libraryItems[i]);
-            }
-
-        }
-    }
-
-}
->>>>>>> origin/master
